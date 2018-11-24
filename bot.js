e037d5fb90ee11ce3e76765bd58ef98767730214
CAmper18 <45084295+CAmper18@users.noreply.github.com>
2018-11-24 20:23:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Lplay | Last Music`,"http://twitch.tv/Death Shop")
+client.user.setGame(`Lplay | ☬ L7Ɲ`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
